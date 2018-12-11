eda2cd3c101318efdf2572f2cf77289b9cda1f3c
unknown <cheneilon@gmail.com>
2018-12-11 18:14:42 +0200

    update ex3

--- Lab3/lab3/ex3/fulladder.v
+++ Lab3/lab3/ex3/fulladder.v
@@ -1,8 +1,17 @@
-module fulladder( sum, co, a, b, ci);
+`include "../ex2/halfadder.v"
+module fulladder(sum, co, a, b, ci);
 
   input   a, b, ci;
   output  sum, co;
 
-  // FILL HERE
-
+  wire hadd1_sum;
+  wire hadd1_co;
+  wire hadd2_sum;
+  wire hadd2_co;
+  
+  halfadder hadd1(a, b, hadd1_sum, hadd1_co);
+  halfadder hadd2(ci, hadd1_sum, hadd2_sum, hadd2_co);
+  
+  sum = hadd2_sum;
+  or g0(co, hadd1_co, hadd2_co);
 endmodule
--- Lab3/lab3/ex3/fulladder_tb.v
+++ Lab3/lab3/ex3/fulladder_tb.v
@@ -1,3 +1,33 @@
 module main;
-  // FILL HERE
+  reg a, b, ci;
+  wire sum, co;
+  fulladder fadd(sum, co, a, b, ci);
+
+  always@(a or b or ci)
+  begin
+    $display("time=%d: %b + %b = %b, carry_in = %b, carry_out = %b\n", $time, a, b, sum, ci, co);
+  end
+
+  initial
+  begin
+    $dumpfile("waves.vcd");
+    $dumpvars;
+    a = 0; b = 0; ci = 0;
+    #5
+    a = 0; b = 1; ci = 0;
+    #5
+    a = 1; b = 0; ci = 0;
+    #5
+    a = 1; b = 1; ci = 0;
+    #5
+    a = 0; b = 0; ci = 1;
+    #5
+    a = 0; b = 1; ci = 1;
+    #5
+    a = 1; b = 0; ci = 1;
+    #5
+    a = 1; b = 1; ci = 1;
+    #5
+    $finish;
+  end
 endmodule
